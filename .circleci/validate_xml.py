e6d0fb67b33ee7fa656b7aa2a74abff7244f86ee
Torsten Sommer <torsten.sommer@3ds.com>
2020-04-18 22:06:55 +0200

    Move build configuration to separate XML file
    see #902

--- .circleci/validate_xml.py
+++ .circleci/validate_xml.py
@@ -1,14 +1,13 @@
 from lxml import etree
 import os
 
-print("Parsing the XSD schema")
+print("Parsing fmiModelDescription schema")
 schema = etree.XMLSchema(file='schema/fmi3ModelDescription.xsd')
-
 parser = etree.XMLParser(schema=schema)
 
 xml_files = [
     'alias_example.xml',
-    'build_configuration.xml',
+    # 'build_configuration.xml',
     'co_simulation.xml',
     'co_simulation_early_return.xml',
     'model_exchange.xml',
@@ -23,3 +22,10 @@ xml_files = [
 for xml_file in xml_files:
     print("Parsing %s" % xml_file)
     etree.parse(os.path.join('docs', 'examples', xml_file), parser)
+
+print("Parsing fmiBuildConfiguration schema")
+schema = etree.XMLSchema(file='schema/fmi3BuildConfiguration.xsd')
+parser = etree.XMLParser(schema=schema)
+
+print("Parsing build_configuration.xml")
+etree.parse(os.path.join('docs', 'examples', 'build_configuration.xml'), parser)
