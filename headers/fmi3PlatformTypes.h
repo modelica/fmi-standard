08388ab3e3b3484b41414b64c2e4bb2ee13867f0
Torsten Sommer <torsten.sommer@3ds.com>
2020-06-26 16:16:32 +0200

    Bump version to 3.0-alpha.4

--- headers/fmi3PlatformTypes.h
+++ headers/fmi3PlatformTypes.h
@@ -3,7 +3,7 @@
 
 /*
 Standard header file to define the argument types of the
-functions of the Functional Mock-up Interface 3.0-alpha.3.
+functions of the Functional Mock-up Interface 3.0-alpha.4.
 This header file must be utilized both by the model and
 by the simulation engine.
 
