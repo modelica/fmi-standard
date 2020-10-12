715fdac31410304689584393e1eff02ddafdaa45
Andreas Junghanns <andreas.junghanns@synopsys.com>
2020-10-12 15:29:19 +0200

    Move Get/Set state descriptions to common

--- headers/fmi3FunctionTypes.h
+++ headers/fmi3FunctionTypes.h
@@ -380,21 +380,25 @@ typedef fmi3Status fmi3SetFMUStateTYPE (fmi3Instance instance, fmi3FMUState  FMU
 typedef fmi3Status fmi3FreeFMUStateTYPE(fmi3Instance instance, fmi3FMUState* FMUState);
 /* end::FreeFMUState[] */
 
-/* tag::SerializedFMUState[] */
+/* tag::SerializedFMUStateSize[] */
 typedef fmi3Status fmi3SerializedFMUStateSizeTYPE(fmi3Instance instance,
                                                   fmi3FMUState  FMUState,
                                                   size_t* size);
+/* end::SerializedFMUStateSize[] */
 
+/* tag::SerializeFMUState[] */
 typedef fmi3Status fmi3SerializeFMUStateTYPE     (fmi3Instance instance,
                                                   fmi3FMUState  FMUState,
                                                   fmi3Byte serializedState[],
                                                   size_t size);
+/* end::SerializeFMUState[] */
 
+/* tag::DeSerializeFMUState[] */
 typedef fmi3Status fmi3DeSerializeFMUStateTYPE   (fmi3Instance instance,
                                                   const fmi3Byte serializedState[],
                                                   size_t size,
                                                   fmi3FMUState* FMUState);
-/* end::SerializedFMUState[] */
+/* end::DeSerializeFMUState[] */
 
 /* Getting partial derivatives */
 /* tag::GetDirectionalDerivative[] */
