4301812a1a1181761ae15105244c0131bd72e014
Christian Bertsch <Christian.Bertsch@de.bosch.com>
2020-10-16 19:48:58 +0200

    Change also in header files

--- headers/fmi3FunctionTypes.h
+++ headers/fmi3FunctionTypes.h
@@ -489,7 +489,7 @@ typedef fmi3Status fmi3SetIntervalFractionTYPE(fmi3Instance instance,
 /* end::SetIntervalFraction[] */
 
 /* tag::NewDiscreteStates[] */
-typedef fmi3Status fmi3NewDiscreteStatesTYPE(fmi3Instance instance,
+typedef fmi3Status fmi3UpdateDiscreteStatesTYPE(fmi3Instance instance,
                                              fmi3Boolean *newDiscreteStatesNeeded,
                                              fmi3Boolean *terminateSimulation,
                                              fmi3Boolean *nominalsOfContinuousStatesChanged,
