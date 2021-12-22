96b884d24d1ae8b49e5313164f4841fb2d8b4a0b
Pierre R. Mai <pmai@pmsf.de>
2021-12-22 19:45:30 +0100

    Rename needsEventHandling to eventHandlingNeeded
    As per suggestion from t-sommer

--- headers/fmi3FunctionTypes.h
+++ headers/fmi3FunctionTypes.h
@@ -606,7 +606,7 @@ typedef fmi3Status fmi3DoStepTYPE(fmi3Instance instance,
                                   fmi3Float64 currentCommunicationPoint,
                                   fmi3Float64 communicationStepSize,
                                   fmi3Boolean noSetFMUStatePriorToCurrentPoint,
-                                  fmi3Boolean* needsEventHandling,
+                                  fmi3Boolean* eventHandlingNeeded,
                                   fmi3Boolean* terminateSimulation,
                                   fmi3Boolean* earlyReturn,
                                   fmi3Float64* lastSuccessfulTime);
