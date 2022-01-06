0b2b6f39289493a4cf1308de3abc7262390c2125
Christian Bertsch <Christian.Bertsch@de.bosch.com>
2022-01-06 08:09:07 +0100

    Update copyright year

--- headers/fmi3PlatformTypes.h
+++ headers/fmi3PlatformTypes.h
@@ -6,7 +6,7 @@ This header file defines the data types of FMI 3.0-beta.3.
 It must be used by both FMU and FMI master.
 
 Copyright (C) 2008-2011 MODELISAR consortium,
-              2012-2021 Modelica Association Project "FMI"
+              2012-2022 Modelica Association Project "FMI"
               All rights reserved.
 
 This file is licensed by the copyright holders under the 2-Clause BSD License
