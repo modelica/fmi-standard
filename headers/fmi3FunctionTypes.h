b483946f201e2def0d96416e0d8a165fc4f3cabd
Torsten Sommer <torsten.sommer@3ds.com>
2022-01-11 23:23:07 +0100

    Update copyright year (#1627)
    Co-authored-by: Christian Bertsch <Christian.Bertsch@de.bosch.com>

--- headers/fmi3FunctionTypes.h
+++ headers/fmi3FunctionTypes.h
@@ -8,7 +8,7 @@ This header file defines the data and function types of FMI 3.0-beta.3.
 It must be used when compiling an FMU or an FMI importer.
 
 Copyright (C) 2011 MODELISAR consortium,
-              2012-2021 Modelica Association Project "FMI"
+              2012-2022 Modelica Association Project "FMI"
               All rights reserved.
 
 This file is licensed by the copyright holders under the 2-Clause BSD License
